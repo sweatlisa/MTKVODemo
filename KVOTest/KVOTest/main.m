5c7ab38fe06bab5549e40f10f21d5f4f106734c8
liyuhong <liyuhonjob@gmail.com>
2014-07-07 18:30:38 +0800

    create the project

---
+++ KVOTest/KVOTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KVOTest
+//
+//  Created by liyuhong on 14-7-7.
+//  Copyright (c) 2014年 liyuhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
